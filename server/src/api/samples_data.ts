c5191c0db56a0e9d8a0123a42804b8ec2419a694
gaspardpeduzzi <gaspard.peduzzi@epfl.ch>
2019-11-16 20:59:57 +0100

    Add dumy data for front testing

--- server/src/api/samples_data.ts
+++ server/src/api/samples_data.ts
@@ -1,5 +1,8 @@
-module.exports = [
-    { "id": "0x0AFF6d90AF529CCc8DaD13A9a36E5A9Ca29B4c3b", "name": "Ulysse", "genre": "animation"},
-    { "id": "0xF9Dfb2b4F11fF829d249Ba0BA1c894CD1b8e022B", "name": "Jean", "genre": "crime"},
-    { "id": "0xf8B75e7D09047de4AAb7EeEcc1fbABCf8D23aa1A", "name": "Julien", "genre": "action"},
+const sample_users = [
+    { "id": "0", "name": "Ulysse", "job": "coder"},
+    { "id": "1", "name": "Jean", "job": "athlete"},
+    { "id": "2", "name": "Julien", "job": "painter"},
 ];
+
+
+export {sample_users}
