ee55d1134521d80998ecccf37b1f10ffc13601a1
ulydev <ulydev@live.fr>
2019-11-16 13:34:32 +0100

    add missing arjs typings

---
+++ app/src/typings/arjs.d.ts
@@ -0,0 +1,10 @@
+/// <reference types="react-scripts" />
+
+declare module JSX {
+    interface IntrinsicElements {
+        "a-scene": any,
+        "a-anchor": any,
+        "a-entity": any,
+        "a-camera-static": any
+    }
+}
