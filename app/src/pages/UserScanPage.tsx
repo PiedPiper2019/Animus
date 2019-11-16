dfa5cc6ff895145795a3c6cf96904493b297382c
ulydev <ulydev@live.fr>
2019-11-16 21:21:59 +0100

    add focused delay

--- app/src/pages/UserScanPage.tsx
+++ app/src/pages/UserScanPage.tsx
@@ -5,18 +5,29 @@ import ButtonDock from '../components/ButtonDock'
 
 class UserScanPage extends Component {
 
-    static HIDE_BUTTON_DOCK_DELAY = 300;
+    static HIDE_BUTTON_DOCK_DELAY = 300
+    static UNFOCUS_ID_DELAY = 200
 
     hideButtonDockTimeout?: NodeJS.Timeout
+    unfocusIdTimeout?: NodeJS.Timeout
+    focusedId?: number
 
     state = {
         buttonDockShown: false
     }
 
     onScanned = (id: number) => {
+        // Button Dock delay
         if (!this.state.buttonDockShown) this.setState({ buttonDockShown: true })
         if (this.hideButtonDockTimeout) clearTimeout(this.hideButtonDockTimeout)
         this.hideButtonDockTimeout = setTimeout(() => this.setState({ buttonDockShown: false }), UserScanPage.HIDE_BUTTON_DOCK_DELAY)
+
+        // Unfocus Id delay
+        if (!this.focusedId) this.focusedId = id
+        if (this.focusedId === id) {
+            if (this.unfocusIdTimeout) clearTimeout(this.unfocusIdTimeout)
+            this.unfocusIdTimeout = setTimeout(() => { this.focusedId = undefined }, UserScanPage.UNFOCUS_ID_DELAY)
+        }
     }
 
     render() {
